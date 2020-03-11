36a3dbfd0a0496b08a1af02a6d6b8314566a6d1e
Aman Michael <aman.david.michael@gmail.com>
2020-03-11 10:40:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,8 @@ class Dog
   def initialize(breed)
     @breed = breed
   end
-  def breed=("Mutt")
-    @breed = "Mutt" 
+  def breed=(Mutt)
+    @breed = Mutt 
   end
   def breed
     @breed = breed
